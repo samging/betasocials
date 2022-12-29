96a4f02b74c6db4a6b7197f3c817d1a87149fc67
s1th <samfuxm@gmail.com>
2022-12-29 23:37:01 +0100

    problem with updating script

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Auth from './pages/auth';
+import Dash from './pages/dash';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+         <Route path='/'  to="" element={<Navigate to="/auth" replace/>} />
+          <Route path="/auth" element={<Auth/>} />
+          <Route path="/dashboard" element={<Dash/>} />
+        </Routes>
+      </BrowserRouter>   
     </div>
   );
 }
