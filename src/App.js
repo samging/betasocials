060d54f6c27d18b93693d98485380ebba1381a04
s1th <samfuxm@gmail.com>
2022-12-30 00:34:40 +0100

    messgae1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Auth from './pages/auth';
-import Dash from './pages/dash';
+import Auth from './pages/auth.tsx';
+import Dash from './pages/dash.tsx';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/react-toastify.css";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <ToastContainer/>
         <Routes>
          <Route path='/'  to="" element={<Navigate to="/auth" replace/>} />
           <Route path="/auth" element={<Auth/>} />
